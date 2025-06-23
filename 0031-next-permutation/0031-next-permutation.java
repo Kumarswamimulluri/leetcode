4ffe4bf820e7706c807ec5e78f2ba879f661d0d4
Kumarswamimulluri <138483928+Kumarswamimulluri@users.noreply.github.com>
2025-06-23 10:57:15 +0530

    Time: 0 ms (100%), Space: 43 MB (87.48%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int ind = -1;
+
+        // finding the decreasing ele from the end
+        for(int i = n-2;i >= 0;i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        // if there is no decresing ele then it will be last permtation simply reverse that
+        if(ind == -1){
+            reverse(nums,0,n-1);
+            return;
+        }
+        // find the permutaion ele just larger than the nums[ind] just swap them
+        for(int i = n-1;i >=0;i--){
+            if(nums[i] > nums[ind]){
+                swap(nums,i,ind);
+                break;
+            }
+        }
+        reverse(nums,ind+1,n-1);
+    }
+    private void reverse(int[] nums,int start,int end){
+        while(start < end){
+            swap(nums,start++,end--);
+        }
+    }
+    private void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+// Intution :
+// Step 1: Find the first decreasing element from the end
+// Step 2: // if there is no decresing ele then it will be last permtation simply reverse that
+// Step 3:  find the permutaion ele just larger than the nums[ind] just swap them
+// fill the remianing as small as possible
+// try to fill the remaining in a sorted way
