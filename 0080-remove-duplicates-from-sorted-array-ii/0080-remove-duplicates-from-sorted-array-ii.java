bae90acc732a966375709481f962c14f78f58b9a
Kumarswamimulluri <138483928+Kumarswamimulluri@users.noreply.github.com>
2026-02-11 11:54:07 +0530

    Time: 0 ms (100%), Space: 48.9 MB (47.39%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,18 +3,30 @@ class Solution {
         int n = nums.length;
         if(n <= 2) return n;
 
-        int i = 2;
-        while(i < n){
-            if(nums[i] == nums[i - 2]){
-                for(int j = i;j < n-1;j++){
-                    nums[j] = nums[j+1];
-                }
-                n--;
-            }
-            else{
-                i++;
+        int ind = 2;
+        for(int i = 2;i < n;i++){
+            if(nums[i] != nums[ind - 2]){
+                nums[ind] = nums[i];
+                ind++;
             }
         }
-        return n;
+        return ind;
     }
 }
+// Brute Force
+// int n = nums.length;
+//         if(n <= 2) return n;
+
+//         int i = 2;
+//         while(i < n){
+//             if(nums[i] == nums[i-2]){
+//                 for(int j = i;j < n-1;j++){
+//                     nums[j] = nums[j+1];
+//                 }
+//                 n--;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         return n;
