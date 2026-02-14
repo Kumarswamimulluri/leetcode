62930a37aad18dc4e397aebe3cf433a1b3b7904a
Kumarswamimulluri <138483928+Kumarswamimulluri@users.noreply.github.com>
2026-02-14 12:01:36 +0530

    Time: 5 ms (97.01%), Space: 43.1 MB (57.04%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,22 +4,43 @@ class Solution {
         int count = 0;
 
         for(int i = 0;i < n;i++){
-            for(int j = i;j < n;j++){
-                if(isPalindrome(s,i,j)){
-                    count++;
-                }
-            }
+            count += expand(s,i,i); //odd
+            count += expand(s,i,i+1); //even
+        }
+        return count;
+
+    }
+    private int expand(String s,int left, int right){
+        int count = 0;
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
         }
         return count;
     }
-    private boolean isPalindrome(String s,int l, int r){
-        while(l < r){
-            if(s.charAt(l) != s.charAt(r)){
-                return false;
-            }
-            l++;
-            r--;
-        }
-        return true;
-    }
 }
+// Brute Force
+
+// int n = s.length();
+//         int count = 0;
+
+//         for(int i = 0;i < n;i++){
+//             for(int j = i;j < n;j++){
+//                 if(isPalindrome(s,i,j)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+//     private boolean isPalindrome(String s,int l, int r){
+//         while(l < r){
+//             if(s.charAt(l) != s.charAt(r)){
+//                 return false;
+//             }
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
